d0274a96097f89a1aca0c5176ac9bed6f0e1664d
Tracie Masek <traciemasek@gmail.com>
2019-06-09 11:25:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,4 +64,22 @@ class TicTacToe
     end 
   end
   
+  def full?
+    @board.all? do |index|
+      index == "X" || index == "O"
+    end
+  end
+  
+  def draw?
+    full? && !won? 
+  end
+  
+def over?(board)
+  won?(board) || draw?(board) || full?(board) 
+end
+  
+def winner(board)
+  won?(board) ? board[won?(board)[0]] : nil
+end
+  
 end 
